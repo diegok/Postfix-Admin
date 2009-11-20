f30f6dad62e0e286c2a88b85950e158ad5717772
Diego Kuperman <diego@freekeylabs.com>
2009-11-20 13:55:49 +0100

    Initial commit, just catapp

---
+++ lib/postadmin.pm
@@ -0,0 +1,64 @@
+package postadmin;
+
+use strict;
+use warnings;
+
+use Catalyst::Runtime 5.80;
+
+# Set flags and add plugins for the application
+#
+#         -Debug: activates the debug mode for very useful log messages
+#   ConfigLoader: will load the configuration from a Config::General file in the
+#                 application's home directory
+# Static::Simple: will serve static files from the application's root
+#                 directory
+
+use parent qw/Catalyst/;
+use Catalyst qw/-Debug
+                ConfigLoader
+                Static::Simple/;
+our $VERSION = '0.01';
+
+# Configure the application.
+#
+# Note that settings in postadmin.conf (or other external
+# configuration file that you set up manually) take precedence
+# over this when using ConfigLoader. Thus configuration
+# details given here can function as a default configuration,
+# with an external configuration file acting as an override for
+# local deployment.
+
+__PACKAGE__->config( name => 'postadmin' );
+
+# Start the application
+__PACKAGE__->setup();
+
+
+=head1 NAME
+
+postadmin - Catalyst based application
+
+=head1 SYNOPSIS
+
+    script/postadmin_server.pl
+
+=head1 DESCRIPTION
+
+[enter your description here]
+
+=head1 SEE ALSO
+
+L<postadmin::Controller::Root>, L<Catalyst>
+
+=head1 AUTHOR
+
+Diego Kuperman
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+1;
